67f0e0d31a81e3c383a03f9ffeff03987de58317
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2025-04-17 10:22:37 +0900

    Update howMuch.java

--- week02/howMuch.java
+++ week02/howMuch.java
@@ -5,14 +5,12 @@ import java.util.*;
  
 public class Main { 
     public static void main(String[] args) throws IOException { 
-        // Scanner를 이용해 입력을 받음 
+   
         Scanner scanner = new Scanner(System.in); 
         int N = scanner.nextInt();  
          
-        // 0부터 9까지의 숫자가 나온 횟수를 저장할 배열 
         int[] count = new int[10]; 
          
-        // 1부터 N까지 반복 
         for (int i = 1; i <= N; i++) { 
             // 현재 숫자를 문자열로 변환하여 각 자릿수를 확인 
             String number = String.valueOf(i); 
@@ -22,7 +20,6 @@ public class Main {
             } 
         } 
  
-        // 결과 출력 
         for (int i = 0; i < 10; i++) { 
             System.out.print(count[i] + \" \"); 
         } 
